4be828a50d6736f013ad282b7fd3bf67e6045865
Sandra <sanruigo@gmail.com>
2013-11-25 14:16:13 +0100

    cambios hechos por sandra

--- src/com/example/prueba_x/MainActivity.java
+++ src/com/example/prueba_x/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.prueba_x;
 
 
-import dataBase.DataBase_vTrainning;
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import dataBase.DataBase_vTrainning;
 
 public class MainActivity extends Activity {
 
